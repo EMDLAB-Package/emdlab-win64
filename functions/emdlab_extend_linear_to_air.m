064ecdeb113b43e00cfd40cb43a4368b542b12ff
EMDLAB Package <alijamalifard1992@gmail.com>
2026-02-17 10:11:59 +0330

    removing fnder function

--- functions/emdlab_extend_linear_to_air.m
+++ functions/emdlab_extend_linear_to_air.m
@@ -4,8 +4,15 @@ mu0 = 4*pi*1e-7;
 nu0 = 1/mu0;
 
 % derivative at last point
-df = fnder(pchip(B,nu));
-slope = ppval(df, B(end));
+pp = pchip(B,nu);
+breaks = pp.breaks;
+coefs  = pp.coefs;   % Each row is [a b c d]
+% Differentiate polynomial coefficients
+% Original: a*(x-xk)^3 + b*(x-xk)^2 + c*(x-xk) + d
+% Derivative: 3a*(x-xk)^2 + 2b*(x-xk) + c
+coefs_der = [3*coefs(:,1), 2*coefs(:,2), coefs(:,3)];
+pp_der = mkpp(breaks, coefs_der);
+slope = ppval(pp_der, B(end));
 
 % distance needed to reach air reluctivity
 DeltaB = (nu0 - nu(end)) / slope;
